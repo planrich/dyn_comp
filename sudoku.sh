c1d3944dd9d8a974d18cd6b3205c859e9b0756cb
Richard Plangger <planrichi@gmail.com>
2013-05-09 09:59:23 +0200

    fixed the issue where the solver did not continue due to returning a valid sudoku

---
+++ sudoku.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# sudoku.sh
+# Copyright (C) 2013 rich <planrichi@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+./cabal-dev/bin/am test/sudoku.am \
+    | awk '$1 ~ /evaluated/ { print $2 };' \
+    | sed -e 's/\]$//; s/\,\]\,/\n/g; s/[\$()\[]//g; s/\,/ /g' \
+    | awk 'BEGIN { r = 0; print "solved sudoku:\n" }; r % 3 == 0 { print '\n'; }; { r++; print $1,$2,$3," ", $4, $5, $6, " ", $7, $8, $9 }'
+
