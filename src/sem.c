0bd6728afa72e13db64d955ed352c8425fe17d35
Richard Plangger <rich@pasra.at>
2014-02-07 19:03:28 +0100

    implemented a simple stackable symbol table

--- src/sem.c
+++ src/sem.c
@@ -158,3 +158,77 @@ bail_out_pat:
     kl_destroy(expr_t, postfix);
     bindings->next = expr;
 }
+
+sym_table_t * neart_sym_table_alloc() {
+    ALLOC_STRUCT(sym_table_t, sym);
+    sym->symbols = kh_init(str_sym_entry_t);
+    sym->parent = sym->next = NULL;
+    return sym;
+}
+
+void neart_sym_table_free(sym_table_t * table) {
+
+    sym_table_t * t = table;
+    sym_table_t * next = table->next;
+
+    while (t != NULL) {
+        kh_destroy(str_sym_entry_t, t->symbols); 
+        free(t);
+
+        t = next;
+        if (t != NULL) {
+            next = next->next;
+        }
+    }
+}
+
+void neart_sym_table_insert(sym_table_t * table, const char * name, sym_entry_t entry) {
+
+    int ret;
+    khint_t k;
+
+    k = kh_put(str_sym_entry_t, table->symbols, name, &ret);
+    if (!ret) {
+        errno = ERR_SYM_ALREADY_DEF;
+        NEART_LOG(LOG_FATAL, "symbol %s is already defined\n", name);
+    } else {
+        kh_value(table->symbols, k) = entry;
+    }
+}
+
+sym_entry_t * neart_sym_table_lookup(sym_table_t * sym, const char * name) {
+
+    sym_table_t * t = sym;
+    sym_table_t * next = t->parent;
+    khint_t k;
+
+    while (t != NULL) {
+
+        k = kh_get(str_sym_entry_t, t->symbols, name);
+        if (k != kh_end(t->symbols)) {
+            return & kh_value(t->symbols, k);
+        }
+
+        t = next;
+        if (t != NULL) {
+            t = t->parent;
+        }
+    }
+
+
+    return NULL;
+}
+
+sym_table_t * neart_sym_table_push(sym_table_t * sym) {
+    sym_table_t * new = neart_sym_table_alloc();
+    new->parent = sym;
+    sym->next = new;
+    return new;
+}
+
+sym_table_t * neart_sym_table_pop(sym_table_t * sym) {
+    sym_table_t * table = sym->parent;
+    neart_sym_table_free(table);
+    return table;
+}
+
--- src/sem.h
+++ src/sem.h
@@ -6,7 +6,78 @@
 
 typedef enum __semantic_error_t {
     ERR_FUNC_ALREADY_DEF,
+    ERR_SYM_ALREADY_DEF,
 } semantic_error_t;
+
+typedef enum __sym_entry_type_t {
+    SYM_FUNC,
+    SYM_VAR,
+
+} sym_entry_type_t;
+
+typedef struct __sym_entry_t {
+    sym_entry_type_t type;
+    union {
+        expr_t * var;
+        func_t * func;
+    };
+} sym_entry_t;
+KHASH_MAP_INIT_STR(str_sym_entry_t, sym_entry_t);
+
+typedef struct __sym_table_t {
+    khash_t(str_sym_entry_t) * symbols;
+    struct __sym_table_t * parent;
+    struct __sym_table_t * next;
+} sym_table_t;
+
+sym_table_t * neart_sym_table_alloc();
+
+/**
+ * Frees all symbol tables and child symbol tables
+ */
+void neart_sym_table_free(sym_table_t * table);
+
+/**
+ * Make a variable visible in the symbol table
+ */
+void neart_sym_table_insert(sym_table_t * table, const char * name, sym_entry_t entry);
+
+/**
+ * Lookup a symbol. This call searches in all parent symbol tables.
+ */
+sym_entry_t * neart_sym_table_lookup(sym_table_t * sym, const char * name);
+
+/**
+ *  +---------+
+ *  | symt 1  |
+ *  +---------+
+ *       ^
+ *       |
+ *       | parent
+ *       |
+ *  +--------+
+ *  | symt 2 |
+ *  +------ -+
+ *
+ *  symt 2 is created and returned
+ */
+sym_table_t * neart_sym_table_push(sym_table_t * sym);
+
+/**
+ *  +---------+
+ *  | symt 1  |
+ *  +---------+
+ *       ^
+ *       |
+ *       | parent
+ *       |
+ *  +--------+
+ *  | symt 2 |
+ *  +------ -+
+ *
+ *  will return only symt 1 and free symt 2
+ */
+sym_table_t * neart_sym_table_pop(sym_table_t * sym);
 
 module_t * neart_check_semantics(compile_context_t * cc, expr_t * root);
 
